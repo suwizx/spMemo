db97dddc7662dee090bc91abab5c9a937be332ec
suwizx <suwijak.pak@gmail.com>
2024-02-05 18:05:18 +0700

    delete api'

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,23 @@
 import PathGrid from "./components/grids/PathGrid";
-import axios from "axios";
+import { store } from "@/app/lib/firebase"
+import { ref , listAll } from 'firebase/storage'
 
 async function datasFetch(){
+  const datasRef = ref(store)
   try{
-    const paths = await axios.get("http://localhost:3000/api/files/paths")
-    return paths.data
-  }
-  catch(err){
-    console.log(err);
-    return err
+      let folder = []
+      const list = await listAll(datasRef)
+      const prefixes = list.prefixes
+      prefixes.forEach((data) => {folder.push(data._location.path_);})
+      return folder
+  }catch(err){
+      return err
   }
 }
           
 export default async function Home() {        
 
-  const data = await datasFetch()
-  const paths = data.paths
+  const paths = await datasFetch()
 
   return (
     <main className="max-w-4xl mx-auto p-4">
